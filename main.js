9b1c8a30851b5ad919eb64bf20c87578133c441a
nguyenthanhlam21 <lamn47593@gmail.com>
2024-01-09 09:32:24 +0700

    first commit'

---
+++ main.js
@@ -0,0 +1,9 @@
+'use strict';   
+const restaurants = {
+    name : 'Classico Italiano',
+    location : 'Via Angelo Tavanti 23, firenze, Italy',
+    categories : ['Italiano', 'Pizzeria', 'Vegetarian', 'Organic'],
+    starterMenu : ['Focaccio', 'Bruschetta', 'Garlic Bread', 'Caprese Salad'],
+    mainMenu : ['Pizza', 'Pasta', 'Risotto'],
+};
+console.log(restaurants);
